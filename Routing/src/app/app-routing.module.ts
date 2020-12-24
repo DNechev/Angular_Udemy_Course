71ae5d108d3bfba617738c8773e9ce2323559d8a
DNechev <dido94m@gmail.com>
2020-12-24 12:40:01 +0100

    Routing
    Passing static data via routing

--- Routing/src/app/app-routing.module.ts
+++ Routing/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -21,11 +23,12 @@ const appRoutes: Routes = [
         component: ServersComponent,
         children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ]},
-    { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: PageNotFoundComponent },
+    { path: 'error', component: ErrorPageComponent, data: {message: 'Page not found!'}},
     // This should always be the last route in the array of routes!!!
-    { path: '**', redirectTo: 'not-found' }
+    { path: '**', redirectTo: 'error' }
   ];
 
 @NgModule({
