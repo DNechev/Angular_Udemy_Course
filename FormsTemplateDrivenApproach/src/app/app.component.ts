c18b58198bb62555a9fb05fa3062622d1792e901
DNechev <dido94m@gmail.com>
2020-12-26 11:16:42 +0100

    Forms handling
    Reseting form afer submission (TD)

--- FormsTemplateDrivenApproach/src/app/app.component.ts
+++ FormsTemplateDrivenApproach/src/app/app.component.ts
@@ -47,5 +47,7 @@ export class AppComponent {
     this.user.secret = this.signUpForm.value.secret;
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
