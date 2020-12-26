95c97cbbdd9c120dcf63e454f03885a131e30c6e
DNechev <dido94m@gmail.com>
2020-12-26 12:06:50 +0100

    Forms Td task solution

--- FormsTdExercice/src/app/app.component.ts
+++ FormsTdExercice/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') form;
+  defaultSubscriptionType = 'Advanced';
+  userData = {
+    email: '',
+    subscriptionType: ''
+  }
 
+  onSubmit() {
+    console.log(this.form.value);
+    this.userData.email = this.form.value.email;
+    this.userData.subscriptionType = this.form.value.subscription;
+    this.form.reset();
+  }
 }
