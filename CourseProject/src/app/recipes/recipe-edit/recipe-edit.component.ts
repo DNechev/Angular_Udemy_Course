209434751ab3e14e69692950e41065bc2acd74d9
DNechev <dido94m@gmail.com>
2020-12-27 18:56:58 +0100

    CourseProject
    Forms- adding Ingredient controls to a form array

--- CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -29,14 +29,32 @@ export class RecipeEditComponent implements OnInit {
   }
 
   private InitForm(): void {
-    const recipeName = this.editMode ? this.recipesService.getRecipeById(this.id).name : '';
-    const imagePath = this.editMode ? this.recipesService.getRecipeById(this.id).imagePath : '';
-    const description = this.editMode ? this.recipesService.getRecipeById(this.id).description : '';
+    const recipe = this.recipesService.getRecipeById(this.id);
+    const recipeName = this.editMode ? recipe.name : '';
+    const imagePath = this.editMode ? recipe.imagePath : '';
+    const description = this.editMode ? recipe.description : '';
+    let recipeIngredients = new FormArray([]);
+
+    if (recipe['ingredients']) {
+      for (const ingredient of recipe.ingredients) {
+        recipeIngredients.push(new FormGroup( {
+          name: new FormControl(ingredient.name),
+          amount: new FormControl(ingredient.amount)
+        }));
+      }
+    }
+
+    console.log(recipeIngredients);
 
     this.recipeForm = new FormGroup({
       recipeName: new FormControl(recipeName, [Validators.required]),
       recipeImagePath: new FormControl(imagePath),
-      recipeDescription: new FormControl(description)
+      recipeDescription: new FormControl(description),
+      ingredients: recipeIngredients,
     });
+  }
+
+  getControls(): any {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
 }
