fb06e0c7a82a209444b7d2b15dfc7f5afbc8bfa0
DNechev <dido94m@gmail.com>
2020-12-29 17:52:27 +0100

    Http module
    Logging interceptor

--- HttpModule/src/app/auth-interceptor.service.ts
+++ HttpModule/src/app/auth-interceptor.service.ts
@@ -5,10 +5,6 @@ export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): any {
     console.log('request is on the way!');
     const modifiedReq = req.clone({headers: req.headers.append('AuthKey', 'xyz')});
-    return next.handle(modifiedReq).pipe(tap( event => {
-      if (event.type === HttpEventType.Response) {
-        console.log('RESPONSE!');
-      }
-    }));
+    return next.handle(modifiedReq);
   }
 }
