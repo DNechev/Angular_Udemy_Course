0608c55edf7311c1eb4d565b5512080fe073f81f
DNechev <dido94m@gmail.com>
2020-12-29 17:36:38 +0100

    Http module
    Using pipe to modify response in interceptors

--- HttpModule/src/app/auth-interceptor.service.ts
+++ HttpModule/src/app/auth-interceptor.service.ts
@@ -1,8 +1,14 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): any {
     console.log('request is on the way!');
-    return next.handle(req);
+    const modifiedReq = req.clone({headers: req.headers.append('AuthKey', 'xyz')});
+    return next.handle(modifiedReq).pipe(tap( event => {
+      if (event.type === HttpEventType.Response) {
+        console.log('RESPONSE!');
+      }
+    }));
   }
 }
