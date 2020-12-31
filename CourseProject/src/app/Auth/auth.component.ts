5721ba6cab3d0ebf470d81dfaeaf06303f4bec06
DNechev <dido94m@gmail.com>
2020-12-31 10:16:43 +0100

    CourseProject
    Setting up sing in/sing up form input

--- CourseProject/src/app/Auth/auth.component.ts
+++ CourseProject/src/app/Auth/auth.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  isLogInMode = true;
+  authForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  onSwitchMode(): void {
+    this.isLogInMode = !this.isLogInMode;
+    this.authForm.reset();
+  }
+
+  onSubmit(): void {
+    console.log('Email: ' + this.authForm.controls.email.value);
+    console.log('Password: ' + this.authForm.controls.password.value);
+    this.authForm.reset();
+  }
 }
