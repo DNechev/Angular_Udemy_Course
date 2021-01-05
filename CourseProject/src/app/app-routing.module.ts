b35be015ab5cad2ce3257f315516ce0a4f62c148
DNechev <dido94m@gmail.com>
2021-01-05 11:37:50 +0100

    CourseProject
    Adding AuthGuard

--- CourseProject/src/app/app-routing.module.ts
+++ CourseProject/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './Auth/auth.component';
+import { AuthGuard } from './Auth/auth.guard';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolverService } from './recipes/recipe-resolver.service';
@@ -11,7 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 const routes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'auth', component: AuthComponent},
-    { path: 'recipes', component: RecipesComponent, children: [
+    { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
       { path: '', component: RecipesStartComponent},
       { path: 'new', component: RecipeEditComponent, resolve: [RecipeResolverService]},
       { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService]},
