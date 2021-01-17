66029860b491d4e6617a9189ccefd90728752e5f
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-17 10:42:13 +0100

    CourseProject
    quickfix

--- CourseProject/src/app/Auth/store/auth.effects.ts
+++ CourseProject/src/app/Auth/store/auth.effects.ts
@@ -32,31 +32,32 @@ export class AuthEffects {
         email: authData.payload.email,
         password: authData.payload.password,
         returnSecureToken: true
-      }).pipe(
+      })
+      .pipe(
         map((responseData: AuthResponseData) => {
           const expDate = new Date(new Date().getTime() + (+responseData.expiresIn * 1000));
           return new authActions.Login({email: responseData.email, userId: responseData.localId,
              idToken: responseData.idToken, expDate: expDate});
         })),
-        catchError((err) => {
+        catchError(errorRes => {
           let errorMessage = 'An unknown error occurred!';
-          if (!err.error || !err.error.error) {
-            return of(new authActions.LoginFail({error: errorMessage}))
-          } else {
-            switch (err.error.error.message) {
-              case 'EMAIL_EXISTS':
-                errorMessage = 'This email already exists!';
-                break;
-                case 'EMAIL_NOT_FOUND':
-                  errorMessage = 'Email is not found!';
-                  break;
-                case 'INVALID_PASSWORD':
-                  errorMessage = 'Invalid password!';
-                  break;
-            }
-            return of(new authActions.LoginFail({error: errorMessage}))
+          if (!errorRes.error || !errorRes.error.error) {
+            return of(new authActions.LoginFail({error: errorMessage}));
           }
-        })    })
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+              break;
+            case 'EMAIL_NOT_FOUND':
+              errorMessage = 'This email does not exist.';
+              break;
+            case 'INVALID_PASSWORD':
+              errorMessage = 'This password is not correct.';
+              break;
+          }
+          return of(new authActions.LoginFail({error: errorMessage}));
+        })
+    })
     // catchError((err) => {
     //   let errorMessage = 'An unknown error occurred!';
     //   if (!err.error || !err.error.error) {
